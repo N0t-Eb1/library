152ea78de52fca001f11041738fbefa30edc8cec
N0t-Eb1 <gangninjax2030@gmail.com>
2025-03-31 02:58:26 -0700

    Create basic template for the app

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ function Book(title, author, pages, readState) {
 function addBookToLibrary() {
     // take params, create a book then store it in the array
 }
+const add = document.querySelector(".place-holder");
+const dialog = document.querySelector("dialog");
+const cancel = document.querySelector(".cancel-dialog");
+
+add.addEventListener("click", () => {
+    dialog.showModal();
+});
+cancel.addEventListener("click", () => {
+    dialog.close();
+});
